abda3b476597edb9a5fb19eec5c9bffa8cef8a73
gigakhrz <kharazishvilig13@gmail.com>
2023-06-13 16:26:48 +0400

    starter code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+function App() {
+  return <AppContainer></AppContainer>;
+}
+
+export default App;
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: #10141e;
+`;
