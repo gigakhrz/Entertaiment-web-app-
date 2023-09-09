719bd826de4374a6d6792d65c02e4d2c1f5641e0
gigakhrz <kharazishvilig13@gmail.com>
2023-09-09 18:19:09 +0400

    add header in app.tsx, If the path is 'Login' or 'SignUp', the header must be hidden.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { styled } from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/LogIn-SignUp/SingUp";
 import LogIn from "./components/LogIn-SignUp/LogIn";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <AppContainer>
+        <Header />
         <Main>
           <Routes>
             <Route path="/" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,26 @@
 import styled from "styled-components";
 import logo from "../../public/images/logo.svg";
 const Header = (): JSX.Element => {
+  //If the path is 'Login' or 'SignUp', the header must be hidden.
+  const path = window.location.pathname;
+  console.log(path);
+
   return (
-    <HeaderContainr>
-      <img src={logo} alt="" />
+    <HeaderContainr path={path}>
+      <img src={logo} alt="Logo img" />
     </HeaderContainr>
   );
 };
 
 export default Header;
 
-const HeaderContainr = styled.header`
-  display: flex;
+const HeaderContainr = styled.header<{ path: string }>`
+  display: ${(props) =>
+    props.path === "/logIn"
+      ? "none"
+      : props.path === "/signUp"
+      ? "none"
+      : "flex"};
   align-items: center;
   justify-content: space-between;
   width: 100%;
