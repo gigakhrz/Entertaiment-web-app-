b18b86aabbb1108639385d7e8df6d5c9d18200ce
gigakhrz <kharazishvilig13@gmail.com>
2023-09-11 20:41:19 +0400

    add allentertainment component in home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,21 @@
 import { styled } from "styled-components";
+import AllEntertainment from "./AllEntertainments";
 
 const Home = (): JSX.Element => {
-  return <></>;
+  return (
+    <HomeWrapper>
+      <AllEntertainment />
+    </HomeWrapper>
+  );
 };
 
 export default Home;
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 24px;
+  padding: 0 16px;
+`;
