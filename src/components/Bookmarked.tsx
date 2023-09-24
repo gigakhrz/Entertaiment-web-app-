da123cbf4e66a04af4faeae1395f8c38a1493052
gigakhrz <kharazishvilig13@gmail.com>
2023-09-24 19:14:27 +0400

    create Bookmarked component  file

---
+++ src/components/Bookmarked.tsx
@@ -0,0 +1,9 @@
+import { useDispatch } from "react-redux";
+import { styled } from "styled-components";
+
+const Bookmarked = (): JSX.Element => {
+  const dispatch = useDispatch();
+  return <></>;
+};
+
+export default Bookmarked;
