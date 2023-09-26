31c825f0e99ef07d2aef9f602cdb1a833bd12a00
gigakhrz <kharazishvilig13@gmail.com>
2023-09-26 17:30:27 +0400

    add map bookmarked Data

--- src/components/Bookmarked.tsx
+++ src/components/Bookmarked.tsx
@@ -1,11 +1,74 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { styled } from "styled-components";
+import { RootState } from "../features/store";
 
 const Bookmarked = (): JSX.Element => {
   const dispatch = useDispatch();
+
+  //all entertainment state
+  const entertainment = useSelector(
+    (store: RootState) => store.entertainment.entertainment
+  );
+
+  // Make sure that the user is logged in
+  const isLoggedIn = useSelector(
+    (store: RootState) => store.isLoggedIn.loggedIn
+  );
+
+  const bookmarkedData = entertainment.filter((item) => {
+    item.isBookmarked === true;
+  });
+
   return (
     <BookmarkWrapper>
       <h4>Bookmarked Movies</h4>
+      {isLoggedIn ? (
+        <div className="mapCont">
+          {bookmarkedData.map((ent, index) => (
+            <div key={index} className="container">
+              <div className="imgCont">
+                <img
+                  className="entImg"
+                  src={ent.thumbnail.regular.small}
+                  alt="image"
+                />
+                <div className="overlay">
+                  <button className="playWrapper">
+                    <img className="playIcon" src={playIcon} alt="play icon " />
+                    <p>Play</p>
+                  </button>
+                </div>
+              </div>
+              <div className="bookmark">
+                <img
+                  onClick={() =>
+                    updateEntertainment(!ent.isBookmarked, ent._id)
+                  }
+                  src={ent.isBookmarked === false ? bookmark : fullbookmark}
+                  className="bookmarkImg"
+                  alt="empty bookmark image"
+                />
+              </div>
+              {/* The bottom side of the element where the title, rating, and other information are located. */}
+              <div className="title">
+                <h6>{ent.year}</h6>
+                <img className="dot" src={dot} alt="" />
+                <img
+                  className="movie"
+                  src={ent.category === "Movie" ? iconMovie : iconSeries}
+                  alt=""
+                />
+                <h6>{ent.category}</h6>
+                <img className="dot" src={dot} alt="" />
+                <h6>{ent.rating}</h6>
+              </div>
+              <p className="name">{ent.title}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
     </BookmarkWrapper>
   );
 };
