3bac2095f289170f3643c01417d9b9cd7bb0a40c
gigakhrz <kharazishvilig13@gmail.com>
2023-09-26 17:20:53 +0400

    add title

--- src/components/Bookmarked.tsx
+++ src/components/Bookmarked.tsx
@@ -3,9 +3,29 @@ import { styled } from "styled-components";
 
 const Bookmarked = (): JSX.Element => {
   const dispatch = useDispatch();
-  return <></>;
+  return (
+    <BookmarkWrapper>
+      <h4>Bookmarked Movies</h4>
+    </BookmarkWrapper>
+  );
 };
 
 export default Bookmarked;
 
-const bookmarkWrapper = styled.div``;
+const BookmarkWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 26px;
+
+  h4 {
+    font-size: 20px;
+    font-weight: 300;
+    letter-spacing: -0.31px;
+    color: white;
+    align-self: baseline;
+  }
+`;
