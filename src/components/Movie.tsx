2f7df030a7782e5145f6b5f7ad22a2051a4c94e6
gigakhrz <kharazishvilig13@gmail.com>
2023-09-30 11:46:32 +0400

    anywahy user is logged in or not map the movies, delete the if statement

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -57,54 +57,48 @@ const Movies = (): JSX.Element => {
   return (
     <TvSerieskWrapper>
       <h4>Movies</h4>
-      {isLoggedIn ? (
-        <div className="mapCont">
-          {movies.map((ent, index) => (
-            <div key={index} className="container">
-              <div className="imgCont">
-                <img
-                  className="entImg"
-                  src={ent.thumbnail.regular.small}
-                  alt="image"
-                />
-                <div className="overlay">
-                  <button className="playWrapper">
-                    <img className="playIcon" src={playIcon} alt="play icon " />
-                    <p>Play</p>
-                  </button>
-                </div>
+
+      <div className="mapCont">
+        {movies.map((ent, index) => (
+          <div key={index} className="container">
+            <div className="imgCont">
+              <img
+                className="entImg"
+                src={ent.thumbnail.regular.small}
+                alt="image"
+              />
+              <div className="overlay">
+                <button className="playWrapper">
+                  <img className="playIcon" src={playIcon} alt="play icon " />
+                  <p>Play</p>
+                </button>
               </div>
-              <div className="bookmark">
-                <img
-                  onClick={() =>
-                    updateEntertainment(!ent.isBookmarked, ent._id)
-                  }
-                  src={ent.isBookmarked === false ? bookmark : fullbookmark}
-                  className="bookmarkImg"
-                  alt="empty bookmark image"
-                />
-              </div>
-              {/* The bottom side of the element where the title, rating, and other information are located. */}
-              <div className="title">
-                <h6>{ent.year}</h6>
-                <img className="dot" src={dot} alt="" />
-                <img
-                  className="movie"
-                  src={ent.category === "Movie" ? iconMovie : iconSeries}
-                  alt=""
-                />
-                <h6>{ent.category}</h6>
-                <img className="dot" src={dot} alt="" />
-                <h6>{ent.rating}</h6>
-              </div>
-              <p className="name">{ent.title}</p>
             </div>
-          ))}
-        </div>
-      ) : (
-        // აქ უნდა ჩაიწეროს ის შემთხვევა როდესაც იუზერი არ არის შესული სისტემაში
-        ""
-      )}
+            <div className="bookmark">
+              <img
+                onClick={() => updateEntertainment(!ent.isBookmarked, ent._id)}
+                src={ent.isBookmarked === false ? bookmark : fullbookmark}
+                className="bookmarkImg"
+                alt="empty bookmark image"
+              />
+            </div>
+            {/* The bottom side of the element where the title, rating, and other information are located. */}
+            <div className="title">
+              <h6>{ent.year}</h6>
+              <img className="dot" src={dot} alt="" />
+              <img
+                className="movie"
+                src={ent.category === "Movie" ? iconMovie : iconSeries}
+                alt=""
+              />
+              <h6>{ent.category}</h6>
+              <img className="dot" src={dot} alt="" />
+              <h6>{ent.rating}</h6>
+            </div>
+            <p className="name">{ent.title}</p>
+          </div>
+        ))}
+      </div>
     </TvSerieskWrapper>
   );
 };
