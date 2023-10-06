d59e9e2a05436f911e5400871e563fe30b045837
gigakhrz <kharazishvilig13@gmail.com>
2023-10-06 15:09:05 +0400

    add filter logic in bookmarked component

--- src/components/Bookmarked.tsx
+++ src/components/Bookmarked.tsx
@@ -9,6 +9,8 @@ import iconSeries from "../../public/images/icon-nav-tv-series.svg";
 import axios from "axios";
 import dot from "../../public/images/Pasted image.png";
 import { fetchEntertainment } from "../App";
+import { useEffect } from "react";
+import { setFilteredEnt } from "../features/filteredEntSlice";
 
 const Bookmarked = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -18,6 +20,11 @@ const Bookmarked = (): JSX.Element => {
   //all entertainment state
   const entertainment = useSelector(
     (store: RootState) => store.entertainment.entertainment
+  );
+
+  //when user filter the entertainment result will save in this state
+  const filteredEnt = useSelector(
+    (store: RootState) => store.filteredEnt.filtered
   );
 
   // Make sure that the user is logged in
@@ -31,7 +38,6 @@ const Bookmarked = (): JSX.Element => {
   );
 
   const bookmarkedData = entertainment.filter((item) => {
-    console.log(item.isBookmarked);
     return item.isBookmarked === true;
   });
 
@@ -53,54 +59,72 @@ const Bookmarked = (): JSX.Element => {
     }
   };
 
-  console.log(bookmarkedData);
+  //based on this value will filter entertainment
+  const inputValue = useSelector((store: RootState) => store.inputValue.value);
+
+  useEffect(() => {
+    const filterEnt = (): void => {
+      let data = bookmarkedData;
+      data = data.filter((item) =>
+        item.title.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      dispatch(setFilteredEnt(data));
+    };
+    filterEnt();
+  }, [inputValue]);
 
   return (
     <BookmarkWrapper>
       <h4>Bookmarked Movies</h4>
       {isLoggedIn ? (
         <div className="mapCont">
-          {bookmarkedData.map((ent, index) => (
-            <div key={index} className="container">
-              <div className="imgCont">
-                <img
-                  className="entImg"
-                  src={ent.thumbnail.regular.small}
-                  alt="image"
-                />
-                <div className="overlay">
-                  <button className="playWrapper">
-                    <img className="playIcon" src={playIcon} alt="play icon " />
-                    <p>Play</p>
-                  </button>
+          {(filteredEnt === undefined ? bookmarkedData : filteredEnt).map(
+            (ent, index) => (
+              <div key={index} className="container">
+                <div className="imgCont">
+                  <img
+                    className="entImg"
+                    src={ent.thumbnail.regular.small}
+                    alt="image"
+                  />
+                  <div className="overlay">
+                    <button className="playWrapper">
+                      <img
+                        className="playIcon"
+                        src={playIcon}
+                        alt="play icon "
+                      />
+                      <p>Play</p>
+                    </button>
+                  </div>
                 </div>
+                <div className="bookmark">
+                  <img
+                    onClick={() =>
+                      updateEntertainment(!ent.isBookmarked, ent._id)
+                    }
+                    src={ent.isBookmarked === false ? bookmark : fullbookmark}
+                    className="bookmarkImg"
+                    alt="empty bookmark image"
+                  />
+                </div>
+                {/* The bottom side of the element where the title, rating, and other information are located. */}
+                <div className="title">
+                  <h6>{ent.year}</h6>
+                  <img className="dot" src={dot} alt="" />
+                  <img
+                    className="movie"
+                    src={ent.category === "Movie" ? iconMovie : iconSeries}
+                    alt=""
+                  />
+                  <h6>{ent.category}</h6>
+                  <img className="dot" src={dot} alt="" />
+                  <h6>{ent.rating}</h6>
+                </div>
+                <p className="name">{ent.title}</p>
               </div>
-              <div className="bookmark">
-                <img
-                  onClick={() =>
-                    updateEntertainment(!ent.isBookmarked, ent._id)
-                  }
-                  src={ent.isBookmarked === false ? bookmark : fullbookmark}
-                  className="bookmarkImg"
-                  alt="empty bookmark image"
-                />
-              </div>
-              {/* The bottom side of the element where the title, rating, and other information are located. */}
-              <div className="title">
-                <h6>{ent.year}</h6>
-                <img className="dot" src={dot} alt="" />
-                <img
-                  className="movie"
-                  src={ent.category === "Movie" ? iconMovie : iconSeries}
-                  alt=""
-                />
-                <h6>{ent.category}</h6>
-                <img className="dot" src={dot} alt="" />
-                <h6>{ent.rating}</h6>
-              </div>
-              <p className="name">{ent.title}</p>
-            </div>
-          ))}
+            )
+          )}
         </div>
       ) : (
         // აქ უნდა ჩაიწეროს ის შემთხვევა როდესაც იუზერი არ არის შესული სისტემაში
